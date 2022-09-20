35d945290ad651e262db5788ae87b17ab63c0842
laxman-boda <laxman.boda96@gmail.com>
2022-09-20 11:02:41 +0530

    Posts reducers with tests

--- src/Components/reducer/index.js
+++ src/Components/reducer/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import SuccessReducer from "./SuccessReducer";
+import posts from './post/reducer'
 
 export default combineReducers({
-    SuccessReducer
+    posts
 })
